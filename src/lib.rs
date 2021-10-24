2bf62204a7ce7e4a97b4caad616beaae8d2d743f
qiujiangkun <qiujiangkun@foxmail.com>
2021-10-24 21:20:41 +0800

    test framework

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,13 @@
 #![no_std]
 
+#![cfg_attr(test, no_main)]
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test::test_runner)]
+#![reexport_test_harness_main = "test_main"]
+
+use core::panic::PanicInfo;
+
+
 pub mod vga;
+pub mod serial;
+pub mod test;
--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,47 @@
 #![no_std]
 #![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(minimal_os::test::test_runner)]
+#![reexport_test_harness_main = "test_main"]
+
+use minimal_os::*;
 
 use core::panic::PanicInfo;
-
-use minimal_os::println;
 
 fn print_message() {
     println!("Hello World!\nYou are my world!");
 }
-/// This function is called on panic.
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    print_message();
+    #[cfg(test)]
+    test_main();
+    panic!("I like panic!");
+}
+#[cfg(not(test))]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     println!("{}", info);
     loop {}
 }
-#[no_mangle]
-pub extern "C" fn _start() -> ! {
-    print_message();
-    panic!("I like panic!");
+
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    test::test_panic_handler(info)
+}
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test_case]
+    fn test_1_plus_1() {
+        serial_println!("Evaluating 1 + 1");
+        assert_eq!(1 + 1, 2);
+    }
+    #[test_case]
+    fn test_1_plus_1_fail() {
+        serial_println!("Evaluating 1 + 1");
+        assert_eq!(1 + 1, 3);
+    }
 }
